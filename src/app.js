0f97b33f6347f497acbe25b892bab6aa1d9480ad
ebelethe <ebelethes@gmail.com>
2026-07-28 14:05:50 -0700

    feat(admin): implement admin dashboard, admin routes with role-based access control and also fix modified some files, the admin panel has been tested with postman and mongo compass which was successful

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";  //allows my frontend to communicate with your backend  
-import router from "./routes/auth/auth.routes.js";
+import authRoutes from "./routes/auth/auth.routes.js";
+import adminRoutes from "./routes/admin/admin.routes.js";
 
 const app=express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", router);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
 
 export default app;
