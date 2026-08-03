3cdaeebd1ea4617ecf57a191bc0dba326781e52d
ebelethe <ebelethes@gmail.com>
2026-08-02 23:08:44 -0700

    Implement API security iwth rate limiting, helmet and cors configuration

---
+++ src/middleware/rateLimiter.middleware.js
@@ -0,0 +1,16 @@
+import { rateLimit } from "express-rate-limit";
+
+export const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+
+    max: 5,
+
+    message: {
+        success: false,
+        message: "Too many requests. Please try again after 15 minutes.",
+    },
+
+    standardHeaders: true,
+
+    legacyHeaders: false,
+});
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,17 +5,18 @@ import  authenticate from "../middleware/auth.middleware.js";
 import {registerUser} from "../controllers/auth/register.controller.js";
 import {loginUser} from "../controllers/auth/login.controller.js";
 import { requestResetPassword, resetPassword, changePassword } from "../controllers/auth/password.controller.js";
+import { authLimiter } from "../middleware/rateLimiter.middleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register",authLimiter, registerUser);
 
-router.post("/login", loginUser);
+router.post("/login",authLimiter, loginUser);
 
-router.post("/reset-password/request", requestResetPassword);
+router.post("/reset-password/request", authLimiter, requestResetPassword);
 
-router.post("/reset-password", resetPassword);
+router.post("/reset-password", authLimiter, resetPassword);
 
-router.put( "/change-password", authenticate, changePassword);
+router.put( "/change-password", authLimiter, authenticate, changePassword);
 
 export default router;
